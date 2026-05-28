c978569075b46490417bcfcf07dafa9269552512
mukeshveeragoni <veeragonimukesh88@gmail.com>
2026-05-28 13:53:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// function greet(a,b){
+//     console.log(a+b);
+// }
+// greet(10,20)
+
+// Anonymous Function	
+// function (){
+//     console.log("Hello");
+// }
+
+// const greet = function(){
+//     console.log("hello");
+// }
+// greet()
+
+//Arrow Function
+// ()=>{
+//     console.log("hello");
+// }
+// const greet1 =()=>{
+//     console.log("hello");
+// }
+// greet1()
+
+//immediate invoke Function
+// function greet2(a,b){
+//     console.log(a+b);
+
+// }(10,20);
+
+
+//Arrays
+// const arr=[1,"hello",true,undefined,null];
+// // console.log(arr)
+// console.log(arr[0])
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i=0 ; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i=0 ; i<arr.length;i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// let sum=0
+// for(let i=0 ; i<arr.length;i++){
+//     sum = sum+i
+// }
+// console.log(sum)
+
+//for each / map
+// const arr=[1,2,3,4]
+// arr.forEach((data)=>{
+//     console.log(data)
+// })
+
+//filter
+// const arr = [1,2,3,4]
+// const even = arr.filter((data)=>{
+//     return data % 2 == 0
+// })
+// console.log(even)
+
+//reduce syntax
+// const arr = [1,2,3,4]
+// arr.reduce((acc,cur)=>{},0)
+
+// const arr = [1,2,3,4]
+// const sum = arr.reduce((acc,cur)=>{
+//     return acc+cur
+// },0)
+// console.log(sum)
+
